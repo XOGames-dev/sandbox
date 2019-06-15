c4a638932c1dc7fea966a0979d74032a4943e887
XOGames-dev <51853566+XOGames-dev@users.noreply.github.com>
2019-06-16 00:31:56 +0200

    Add files via upload

--- timer.js
+++ timer.js
@@ -4,4 +4,23 @@
  * and open the template in the editor.
  */
 
+var countDownDate = new Date("Apr 16, 2020 00:00:00").getTime();
+// Update the count down every 1 second
+var x = setInterval(function() {
 
+  var now = new Date().getTime();
+  var distance = countDownDate - now;
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = days + "d " + hours + "h "
+  + minutes + "m " + seconds + "s ";
+
+  // If the count down is finished, write some text 
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("time").innerHTML = "Chilmon dalej nie kupił lapka";
+  }
+}, 1000);
