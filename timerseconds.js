5a2c725226c8961aff75c0de91628cba860e6085
XOGames-dev <51853566+XOGames-dev@users.noreply.github.com>
2019-06-16 01:16:18 +0200

    Update timerseconds.js

--- timerseconds.js
+++ timerseconds.js
@@ -12,5 +12,5 @@ var now = new Date().getTime();
 var distance = countDownDate - now;
 var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 document.getElementById("timerseconds").innerHTML = seconds + "s ";
-} ,100000;
+} ,100000);
 
