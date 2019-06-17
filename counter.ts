d31ffd58fa4fd118750f4987ee9409c1ecaca804
XOGames-dev <51853566+XOGames-dev@users.noreply.github.com>
2019-06-17 14:52:57 +0200

    Add files via upload

---
+++ counter.ts
@@ -0,0 +1,11 @@
+﻿let text: string = document.getElementById(text);
+let i: number;
+let w: number = 0;
+
+for (i = 0; i <= text.length; i++)
+{
+    if (string[i] == " ")
+        w++;
+}
+document.getElementById(result) = w;
+
